e87b72c460a1acc2a6e54ba0a696290abbd12c1e
emmanlemon <emmanuellemon111200@gmail.com>
2022-07-20 08:51:28 +0800

    Create Todo

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TodoController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+route::redirect('/' ,'/todo');
+
+Route::controller(TodoController::class)->group(function(){
+    route::get('/todo' , 'index')->name('index');
+    // route::get('/blog/create', 'create')->name('create');
+    route::post('/todo', 'store')->name('store');
+    // route::post('/update' , 'update')->name('update');
+    // route::delete('blog/{blog}' , 'destroy')->name('destroy');
+    // route::get('update/{id}' , 'edit');
+    // route::get('blog/view/{id}' , 'show');
 });
